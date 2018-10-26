1004a37578b683df4ad60642e28437273454395e
Devon Kemena <noved98@web.de>
2018-10-27 00:30:04 +0200

    changed spawn running mechanik

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var roleRanger = require('role.ranger');
 require('prototype.spawn')();
 
 module.exports.loop = function () {
+  Game.spawns["Spawn1"].currentSpawnCreepsAndEnergy();
   /*
   //Führt alle 10 Ticks aus
   Game.spawns.Spawn1.memory.TicksToWaitForStatus = Game.spawns.Spawn1.memory.TicksToWaitForStatus-1;
@@ -49,13 +50,12 @@ module.exports.loop = function () {
     }
   }
 
-//new script
-if(false){ //temp
-//Set Minimum creeps
-Game.spawns.Spawn1.setMinRoles(5, 4, 2, 0, 0);
+  //Set Minimum creeps
+  Game.spawns.Spawn1.setMinRoles(4, 4, 3, 0, 0);
 
-//Run thorugh all Spawns
+  //Run thorugh all Spawns
   for(let currentSpawn in Game.spawns){
+
     //Run Creeps
     for(let name in Game.creeps) {
       let creep = Game.creeps[name];
@@ -76,6 +76,7 @@ Game.spawns.Spawn1.setMinRoles(5, 4, 2, 0, 0);
       }
     }
 
+    //Info about Spawn and its Energy
     Game.spawns[currentSpawn].currentSpawnCreepsAndEnergy();
 
     //Energy
@@ -87,82 +88,23 @@ Game.spawns.Spawn1.setMinRoles(5, 4, 2, 0, 0);
       {align: 'left', opacity: 0.8});
 
     //Respawn Creeps if insufficient
-    if(Game.spawns["Spawn1"].memory.currentCreepAmount < Game.spawns["Spawnn1"].memory.numberOfSetMinimumCreeps){
+    if(Game.spawns[currentSpawn].memory.currentCreepAmount < Game.spawns[currentSpawn].memory.numberOfSetMinimumCreeps){
 
-      if(Game.spawns[currentSpawnTemp].memory.currentHarvesters) {
-        Game.spawns[currentSpawn].spawnHarvester(currentSpawn, 200);
+      if(Game.spawns[currentSpawn].memory.currentHarvesters) {
+        Game.spawns[currentSpawn].spawnHarvester(currentSpawn, 550);
       }
-      else if(Game.spawns[currentSpawnTemp].memory.currentUpgrader) {
-
+      else if(Game.spawns[currentSpawn].memory.currentUpgrader) {
+        Game.spawns[currentSpawn].spawnUpgrader()(currentSpawn, 550);
       }
-      else if(Game.spawns[currentSpawnTemp].memory.currentBuilder) {
-
+      else if(Game.spawns[currentSpawn].memory.currentBuilder) {
+        Game.spawns[currentSpawn].spawnBuilder()()(currentSpawn, 550);
       }
-      else if(Game.spawns[currentSpawnTemp].memory.currentMeleeAttacker) {
-
+      else if(Game.spawns[currentSpawn].memory.currentMeleeAttacker) {
+        Game.spawns[currentSpawn].spawnMeleeAttacker()()(currentSpawn, 500);
       }
-      else if(Game.spawns[currentSpawnTemp].memory.currentRanger) {
-
+      else if(Game.spawns[currentSpawn].memory.currentRanger) {
+        Game.spawns[currentSpawn].spawnRanger()()(currentSpawn, 200);
       }
-
     }
-
   }
-
-}
-
-
-  var requiredEnergyToSpawn = 300;
-  var numberOfMinimumHarvesters = 3;
-  var numberOfMinimumUpgraders = 2;
-  var numberOfMinimumBuilder  = 4;
-  var numberOfMinimumMeleeAttacker  = 0;
-  var numberOfMinimumRanger = 0;
-
-
-  var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-  var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-  var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-  var meleeAttacker = _.filter(Game.creeps, (creep) => creep.memory.role == 'meleeAttacker');
-  var ranger = _.filter(Game.creeps, (creep) => creep.memory.role == 'ranger');
-
-  if(Game.spawns.Spawn1.room.energyAvailable  >= requiredEnergyToSpawn) {
-
-       if(harvesters.length < numberOfMinimumHarvesters) {
-          var newName = 'Harvester' + Game.time;
-          //console.log('Spawning new harvester: ' + newName);
-          Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-          {memory: {role: 'harvester', harvesting: false, moving: true, target: null}});
-      }
-      else if(upgrader.length < numberOfMinimumUpgraders) {
-          var newName = 'Upgrader' + Game.time;
-          //console.log('Spawning new upgrader: ' + newName);
-          Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-          {memory: {role: 'upgrader', harvesting: false, moving: true, target: null}});
-      }
-      else if(builder.length < numberOfMinimumBuilder) {
-          var newName = 'Builder' + Game.time;
-          //console.log('Spawning new builder: ' + newName);
-          Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName,
-          {memory: {role: 'builder', harvesting: false, moving: true, repair_: false, target: null}});
-      }
-       else if(meleeAttacker.length < numberOfMinimumMeleeAttacker) {
-          var newName = 'MeleeAttacker' + Game.time;
-          //console.log('Spawning new meleeAttacker: ' + newName);
-          Game.spawns['Spawn1'].spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, MOVE, MOVE, ATTACK, ATTACK, MOVE], newName,
-          {memory: {role: 'meleeAttacker', moving: true, target: null}});
-      }
-      else if(ranger.length < numberOfMinimumRanger) {
-		//Zusatzbedingung
-		if(Game.spawns['Spawn1'].room.energyAvailable > Game.spawns['Spawn1'].room.energyCapacityAvailable - 100) {
-			var newName = 'Ranger' + Game.time;
-			//console.log('Spawning new blocker: ' + newName);
-			Game.spawns['Spawn1'].spawnCreep([RANGED_ATTACK, MOVE], newName,
-			{memory: {role: 'ranger', moving: true, target: null}});
-		}
-      }
-
-  }
-
-
 }
